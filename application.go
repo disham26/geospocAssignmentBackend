0ca9674b00200589fadbac9342e8e1b6019fd120
prasang <prasang@ruvi.in>
2019-03-18 16:20:37 +0530

    url update

--- application.go
+++ application.go
@@ -12,14 +12,14 @@ import (
 func main() {
 	STATIC_DIR := "/uploads/"
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/submitReview", controllers.SubmitReview).Methods("POST")
-	router.HandleFunc("/checkEmail/{email}", controllers.CheckEmailExists).Methods("GET")
-	router.HandleFunc("/checkUser/{email}/{password}", controllers.ValidateUser).Methods("GET")
-	router.HandleFunc("/approval/{email}/{password}", controllers.ApproveUser).Methods("GET")
-	router.HandleFunc("/getAllProfiles", controllers.GetAllProfiles).Methods("GET")
-	router.HandleFunc("/getProfile/{email}", controllers.GetProfileById).Methods("GET")
-	router.HandleFunc("/addComment/{emailOfProfile}/{comment}/{by}", controllers.AddComment).Methods("GET")
-	router.HandleFunc("/addRating/{emailOfProfile}/{rating}/{by}", controllers.AddRating).Methods("GET")
+	router.HandleFunc("/api/submitReview", controllers.SubmitReview).Methods("POST")
+	router.HandleFunc("/api/checkEmail/{email}", controllers.CheckEmailExists).Methods("GET")
+	router.HandleFunc("/api/checkUser/{email}/{password}", controllers.ValidateUser).Methods("GET")
+	router.HandleFunc("/api/approval/{email}/{password}", controllers.ApproveUser).Methods("GET")
+	router.HandleFunc("/api/getAllProfiles", controllers.GetAllProfiles).Methods("GET")
+	router.HandleFunc("/api/getProfile/{email}", controllers.GetProfileById).Methods("GET")
+	router.HandleFunc("/api/addComment/{emailOfProfile}/{comment}/{by}", controllers.AddComment).Methods("GET")
+	router.HandleFunc("/api/addRating/{emailOfProfile}/{rating}/{by}", controllers.AddRating).Methods("GET")
 	router.
 		PathPrefix(STATIC_DIR).
 		Handler(http.StripPrefix(STATIC_DIR, http.FileServer(http.Dir("."+STATIC_DIR))))
